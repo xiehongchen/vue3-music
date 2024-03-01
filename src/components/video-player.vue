6cb9470694c53e47f56491ff8531b264798b9ad2
xiehongchen <1754581057@qq.com>
2024-03-01 11:22:08 +0800

    视频播放器

---
+++ src/components/video-player.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="video-player" id="mse"></div>
+</template>
+
+<script setup lang='ts'>
+import Player from 'xgplayer'
+import 'xgplayer/dist/index.min.css'
+const props = defineProps({
+  url: {
+    type: String,
+    default: ''
+  },
+  poster: {
+    type: String,
+    default: ''
+  }
+
+})
+const initPlayer = () => {
+  if (!props.url) return
+  new Player({
+    id: 'mse',
+    url: props.url,
+    poster: props.poster,
+    videoInit: true,
+    lang: 'zh-cn',
+    width: "100%"
+  })
+}
+
+onMounted(() => {
+  initPlayer()
+})
+
+watch(() => props.url, (newUrl, oldUrl) => {
+  if (newUrl && newUrl !== oldUrl) {
+    initPlayer()
+  } else {
+    
+  }
+})
+</script>
+
+<style scoped lang='scss'>
+
+</style>
--- src/views/mv/index.vue
+++ src/views/mv/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+    <div class="mv">
+    <div class="mv-content">
+      <div class="left">
+        <p class="title">mv详情</p>
+
+        <div class="player">
+          <!-- <VideoPlayer
+            :url="mvPlayInfo.url"
+            :poster="mvDetail.cover"
+            ref="video"
+          /> -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +21,81 @@
 </script>
 
 <style lang="scss" scoped>
+.mv {
+  padding: 16px 32px;
 
+  .title {
+    margin-bottom: 16px;
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  .mv-content {
+    display: flex;
+    max-width: 950px;
+    margin: auto;
+
+    .left {
+      flex: 1;
+
+      .player {
+        margin-bottom: 16px;
+        overflow: hidden;
+        border-radius: 4px;
+      }
+
+      .author-wrap {
+        display: flex;
+        align-items: center;
+        margin-bottom: 32px;
+
+        .avatar {
+          @include img-wrap(60px);
+          margin-right: 8px;
+          img {
+            @include round(60px);
+          }
+        }
+
+        .author {
+          font-size: 20px;
+        }
+      }
+
+      .name {
+        margin-bottom: 16px;
+        font-size: 20px;
+        font-weight: 700;
+      }
+
+      .desc {
+        display: flex;
+        color: #BEBEBE;
+        font-size: 13px;
+
+        .date {
+          display: inline-block;
+          margin-right: 24px;
+        }
+      }
+
+      .comments {
+        margin-top: 48px;
+      }
+    }
+
+    .right {
+      width: 36%;
+      padding-left: 32px;
+
+      .simi-mvs {
+        padding: -8px 0;
+
+        .simi-mv-card {
+          padding: 8px 0;
+        }
+      }
+    }
+  }
+}
 </style>
